6c09f1f88cb60498ad511ef2a00f85deeba8801d
Yukun Su <yukun.su@ericsson.com>
2014-06-30 14:04:51 -0400

    test

--- src/test/java/org/jenkinsci/plugins/envinject/EnvInjectBuildWrapperTest.java
+++ src/test/java/org/jenkinsci/plugins/envinject/EnvInjectBuildWrapperTest.java
@@ -2,26 +2,48 @@ package org.jenkinsci.plugins.envinject;
 
 import hudson.Util;
 import hudson.model.FreeStyleBuild;
+import hudson.model.Result;
 import hudson.model.FreeStyleProject;
 import hudson.model.Hudson;
-import hudson.model.Result;
-import junit.framework.Assert;
-import org.jenkinsci.lib.envinject.EnvInjectAction;
-import org.jvnet.hudson.test.HudsonTestCase;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import jenkins.model.Jenkins;
+import junit.framework.Assert;
+
+import org.jenkinsci.lib.envinject.EnvInjectAction;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.HudsonTestCase;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.apache.commons.io.FileUtils;
+import hudson.model.*;
+import hudson.tasks.Shell;
+import org.junit.Test;
 
 /**
  * @author Gregory Boissinot
  */
 public class EnvInjectBuildWrapperTest extends HudsonTestCase {
+    @Rule public JenkinsRule j = new JenkinsRule();
 
+    @Test
     public void testPropertiesContentCustomWorkspace() throws Exception {
 
         String customWorkspaceValue = Hudson.getInstance().getRootPath().getRemote() + "/customWorkspace";
         String customEnvVarName = "materialize_workspace_path";
         String customEnvVarValue = "${WORKSPACE}/materialize_workspace";
+        
+        /**
+         * Testing
+         */
+        String customWorkspaceValue2 = Jenkins.getInstance().getRootPath().getRemote() + "/customWorkspace";
+        System.out.println("hudson: " + customWorkspaceValue);
+        System.out.println("Jenkins: " + customWorkspaceValue2);
+        System.out.println("Jenkins root path: " + Jenkins.getInstance().getRootPath());
+        System.out.println("getRootDir: " + Jenkins.getInstance().getRootDir());
+        System.out.println("getRootUrl: " + Jenkins.getInstance().getRootUrl());
 
         FreeStyleProject project = createFreeStyleProject();
         project.setCustomWorkspace(customWorkspaceValue);
@@ -44,7 +66,6 @@ public class EnvInjectBuildWrapperTest extends HudsonTestCase {
         Map<String, String> mapEnvVars = new HashMap<String, String>();
         mapEnvVars.put("WORKSPACE", buildWorkspaceValue);
         String expectedCustomEnvVarValue = resolveVars(customEnvVarValue, mapEnvVars);
-
         //2-- Get injected value for the specific variable
         EnvInjectAction envInjectAction = build.getAction(EnvInjectAction.class);
         Assert.assertNotNull(envInjectAction);
@@ -52,7 +73,21 @@ public class EnvInjectBuildWrapperTest extends HudsonTestCase {
         Assert.assertNotNull(envVars);
         String resolvedValue = envVars.get(customEnvVarName);
         Assert.assertNotNull(resolvedValue);
-
+        
+        /**
+         * Testing
+         */
+        System.out.println("build get url: " + build.getUrl());
+        System.out.println("build get work space: " + build.getWorkspace());
+        System.out.println("build get root dir: " + build.getRootDir().getPath());
+        System.out.println("build work space value: " + buildWorkspaceValue);
+        System.out.println("expected: " + expectedCustomEnvVarValue);
+        //resolvedValue = envVars.get("JENKINS_HOME") + "/customWorkspace/materialize_workspace"; 
+        System.out.println("JENKINS_HOME: " + envVars.get("JENKINS_HOME"));
+        System.out.println("HUDSON_HOME: " + envVars.get("HUDSON_HOME"));
+        System.out.println("materialize_workspace_path: " + envVars.get("materialize_workspace_path"));
+        System.out.println("WORKSPACE: " + envVars.get("WORKSPACE"));
+        
         //3-- Test equals
         Assert.assertEquals(expectedCustomEnvVarValue, resolvedValue);
     }
